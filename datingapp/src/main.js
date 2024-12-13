8e9d6f7773063a05078edfbe06da04487f9af414
Ch3mie <ryanc668@nycstudents.net>
2024-12-13 12:26:36 -0500

    e

--- datingapp/src/main.js
+++ datingapp/src/main.js
@@ -1,1 +1,40 @@
 import "./style.css";
+import { people } from "./people.js";
+import { DOMSelectors } from "./DOM.js";
+
+function Displaypeople(people) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="card">
+        <h2>${person.name}</h2>
+        <h4>${person.age}, ${person.race}</h4>
+        <h4>${person.gender} | exes: ${person.exes}</h4>
+        <img class="imagesize" src="${person.image}" alt="e" />
+      </div>
+    `
+  );
+}
+
+function ILikey() {
+  Ilikebutton.addEventListener("click", (event) => {
+    event.preventDefault();
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+          <h2>${person.name}</h2>
+          <h4>${person.age}, ${person.race}</h4>
+          <h4>${person.gender} | exes: ${person.exes}</h4>
+          <img class="imagesize" src="${person.image}" alt="e" />
+        </div>
+      `
+    );
+})
+}
+
+function INoLikey() {
+  Ilikebutton.addEventListener("click", (event) => {
+    event.preventDefault();
+    //Make something to remove the card from the array pool
+}
