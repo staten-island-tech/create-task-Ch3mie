f3b5625ba27f53047b19721a75ac3b96bb8f3a39
Ch3mie <ryanc668@nycstudents.net>
2024-12-18 12:30:20 -0500

    e

--- datingapp/src/DOM.js
+++ datingapp/src/DOM.js
@@ -2,3 +2,4 @@ const DOMSelectors = {
   container: document.querySelector(".container"),
   container2: document.querySelector(".container2"),
 };
+export { DOMSelectors };
--- datingapp/src/main.js
+++ datingapp/src/main.js
@@ -8,17 +8,26 @@ Displaypeople(people);
 
 function Displaypeople(people) {
   let person = people[index];
-  DOMSelectors.container2.insertAdjacentHTML(
+  DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     ` 
-      <div class="card">
-        <h2>${person.name}</h2>
-        <h4>${person.age}, ${person.race}</h4>
-        <h4>${person.gender} | exes: ${person.exes}</h4>
-        <img class="imagesize" src="${person.image}" alt="e" />
-         <button class="Ilikebutton">Dylan goons</button>
-          <button class="Inolikebutton">Dylan dosent goon</button>
-      </div>
+    <div class="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="${person.image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${person.name}</h2>
+    <p>${person.race}</p>
+    <p>${person.gender}</p>
+    <p>${person.age}</p>
+    <p>${person.exes}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
     `
   );
   index++;
@@ -27,7 +36,7 @@ function Displaypeople(people) {
 function ILikey() {
   Ilikebutton.addEventListener("click", (event) => {
     event.preventDefault();
-    DOMSelectors.container.insertAdjacentHTML(
+    DOMSelectors.container2.insertAdjacentHTML(
       "beforeend",
       `
         <div class="card">
