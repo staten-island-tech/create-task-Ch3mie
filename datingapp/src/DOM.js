9d6eba2283b82c7fc439f42f4fed2c08e4d164b8
Ch3mie <ryanc668@nycstudents.net>
2024-12-19 12:32:22 -0500

    e

--- datingapp/src/DOM.js
+++ datingapp/src/DOM.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
   container2: document.querySelector(".container2"),
+  Likebutton: document.querySelector(".Likebutton"),
 };
 export { DOMSelectors };
--- datingapp/src/main.js
+++ datingapp/src/main.js
@@ -3,10 +3,10 @@ import { people } from "./people.js";
 import { DOMSelectors } from "./DOM.js";
 
 let index = 0;
-
 Displaypeople(people);
 
 function Displaypeople(people) {
+  console.log(index);
   let person = people[index];
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
@@ -24,35 +24,55 @@ function Displaypeople(people) {
     <p>${person.age}</p>
     <p>${person.exes}</p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <button class="NoLikebutton btn btn-primary">No like</button>
+      <button class="Likebutton btn btn-primary">Like</button>
     </div>
   </div>
 </div>
     `
   );
-  index++;
+  ILikey(person);
+  INoLikey();
 }
 
-function ILikey() {
-  Ilikebutton.addEventListener("click", (event) => {
+function ILikey(person) {
+  const Likebutton = document.querySelector(".Likebutton");
+  Likebutton.addEventListener("click", (event) => {
     event.preventDefault();
+    DOMSelectors.container.innerHTML = "";
     DOMSelectors.container2.insertAdjacentHTML(
       "beforeend",
       `
-        <div class="card">
-          <h2>${person.name}</h2>
-          <h4>${person.age}, ${person.race}</h4>
-          <h4>${person.gender} | exes: ${person.exes}</h4>
-          <img class="imagesize" src="${person.image}" alt="e" />
-        </div>
+         <div class="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="${person.image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${person.name}</h2>
+    <p>${person.race}</p>
+    <p>${person.gender}</p>
+    <p>${person.age}</p>
+    <p>${person.exes}</p>
+    <div class="card-actions justify-end">
+   
+    </div>
+  </div>
+</div>
       `
     );
+    index++;
+    Displaypeople(people);
   });
 }
 
 function INoLikey(name) {
-  Inolikebutton.addEventListener("click", (event) => {
+  const NoLikebutton = document.querySelector(".NoLikebutton");
+  NoLikebutton.addEventListener("click", (event) => {
     event.preventDefault();
-    cardArray = cardArray.filter((item) => item.name !== name);
+    DOMSelectors.container.innerHTML = "";
+    index++;
+    Displaypeople(people);
   });
 }
