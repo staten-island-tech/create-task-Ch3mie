fbcdf6a9c9bce4945179ad7c5dcaa80c3d239c71
Ch3mie <ryanc668@nycstudents.net>
2025-01-02 12:30:07 -0500

    e

--- datingapp/src/main.js
+++ datingapp/src/main.js
@@ -4,13 +4,38 @@ import { DOMSelectors } from "./DOM.js";
 
 let index = 0;
 Displaypeople(people);
+let matches = [];
 
 function Displaypeople(people) {
   console.log(index);
   let person = people[index];
-  DOMSelectors.container.insertAdjacentHTML(
-    "beforeend",
-    ` 
+  console.log(person);
+  if (person == undefined) {
+    matches.forEach((person) =>
+      DOMSelectors.container.insertAdjacentHTML(
+        "beforeend",
+        ` 
+    <div class="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="${person.image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${person.name}</h2>
+    <p>${person.race}</p>
+    <p>${person.gender}</p>
+    <p>${person.age}</p>
+    <p>${person.exes}</p>
+  </div>
+</div>
+    `
+      )
+    );
+  } else {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      ` 
     <div class="card bg-base-100 w-96 shadow-xl">
   <figure>
     <img
@@ -30,17 +55,19 @@ function Displaypeople(people) {
   </div>
 </div>
     `
-  );
-  ILikey(person);
-  INoLikey();
+    );
+    ILikey(person);
+    INoLikey();
+  }
 }
 
 function ILikey(person) {
   const Likebutton = document.querySelector(".Likebutton");
   Likebutton.addEventListener("click", (event) => {
     event.preventDefault();
+    matches.push(person);
     DOMSelectors.container.innerHTML = "";
-    DOMSelectors.container2.insertAdjacentHTML(
+    /* DOMSelectors.container2.insertAdjacentHTML(
       "beforeend",
       `
          <div class="card bg-base-100 w-96 shadow-xl">
@@ -61,7 +88,7 @@ function ILikey(person) {
   </div>
 </div>
       `
-    );
+    ); */
     index++;
     Displaypeople(people);
   });
