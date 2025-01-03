e6f4c28cd11f64e661def31801840dc2e9075753
Ch3mie <ryanc668@nycstudents.net>
2025-01-03 12:20:12 -0500

    e

--- datingapp/src/main.js
+++ datingapp/src/main.js
@@ -10,6 +10,8 @@ function Displaypeople(people) {
   console.log(index);
   let person = people[index];
   console.log(person);
+  DOMSelectors.container.className =
+    "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center";
   if (person == undefined) {
     matches.forEach((person) =>
       DOMSelectors.container.insertAdjacentHTML(
@@ -33,6 +35,8 @@ function Displaypeople(people) {
       )
     );
   } else {
+    DOMSelectors.container.className =
+      "flex justify-center items-center min-h-screen";
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
       ` 
@@ -67,28 +71,7 @@ function ILikey(person) {
     event.preventDefault();
     matches.push(person);
     DOMSelectors.container.innerHTML = "";
-    /* DOMSelectors.container2.insertAdjacentHTML(
-      "beforeend",
-      `
-         <div class="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src="${person.image}"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${person.name}</h2>
-    <p>${person.race}</p>
-    <p>${person.gender}</p>
-    <p>${person.age}</p>
-    <p>${person.exes}</p>
-    <div class="card-actions justify-end">
-   
-    </div>
-  </div>
-</div>
-      `
-    ); */
+
     index++;
     Displaypeople(people);
   });
